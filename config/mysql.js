37c316b0ba105b169d619a1f0a8d05943eba1c28
nghdavid <35591407+nghdavid@users.noreply.github.com>
2022-11-06 22:35:48 +0800

    fix: Change env name

--- config/mysql.js
+++ config/mysql.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise'); // using asynchronous version of mysql2
 
 const options = {
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
