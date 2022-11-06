36f207678d81774b4dba243ec574fadd97ea5804
nghdavid <35591407+nghdavid@users.noreply.github.com>
2022-11-06 16:37:19 +0800

    feat: Add redis cluster config

---
+++ config/redis-cluster.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const Redis = require('ioredis');
+
+const { REDIS_PORT_1, REDIS_HOST_1, REDIS_USERNAME_1, REDIS_PASSWORD_1, REDIS_PORT_2, REDIS_HOST_2, REDIS_USERNAME_2, REDIS_PASSWORD_2 } = process.env;
+
+const pubClient = new Redis.Cluster([
+    {
+        port: REDIS_PORT_1,
+        host: REDIS_HOST_1,
+        username: REDIS_USERNAME_1,
+        password: REDIS_PASSWORD_1,
+    },
+    {
+        port: REDIS_PORT_2,
+        host: REDIS_HOST_2,
+        username: REDIS_USERNAME_2,
+        password: REDIS_PASSWORD_2,
+    },
+]);
+
+const subClient = pubClient.duplicate();
+
+pubClient.on('connect', () => {
+    console.log('Redis connected!');
+});
+
+pubClient.on('error', (error) => {
+    console.log('Redis connection error:', error);
+});
+
+module.exports = {
+    pubClient,
+    subClient,
+};
