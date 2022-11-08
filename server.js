13b66b1bb448324b658d40b06848e81e06e0aa1d
nghdavid <nghdavid123@gmail.com>
2022-11-08 15:53:11 +0800

    refactor: Simplify time check
    將檢查時間改成用class的方式
    以增加可讀性

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
 require('dotenv').config();
 const process = require('process');
-const { DateTime } = require('luxon');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
 const app = require('./app');
 const httpServer = createServer(app);
+const ActivityClass = require('./util/activity');
+const Activity = new ActivityClass();
 
-const { PORT_TEST, PORT, NODE_ENV, TIME_ZONE_DIFF } = process.env;
+const { PORT_TEST, PORT, NODE_ENV } = process.env;
 const port = NODE_ENV == 'test' ? PORT_TEST : PORT;
-if (TIME_ZONE_DIFF === undefined) {
-    console.error('No env');
-    process.exit(0);
-}
-const YEAR = process.argv[2];
-const MONTH = process.argv[3];
-const Date = process.argv[4];
-const HOUR = process.argv[5];
-const MINUTE = process.argv[6];
 
-const timeZoneDiff = Number(TIME_ZONE_DIFF);
-const startTime = DateTime.local(Number(YEAR), Number(MONTH), Number(Date), Number(HOUR - timeZoneDiff), Number(MINUTE)).setZone('Asia/Taipei');
 const url = 'http://localhost:3000/miaosha.html';
 const password = 'th';
 
@@ -32,10 +22,10 @@ const io = new Server(httpServer, {
         credentials: true,
     },
 });
+
 function informUser() {
-    const now = DateTime.now().setZone('Asia/Taipei');
     // console.log('Waiting');
-    if (now > startTime) {
+    if (Activity.isStart()) {
         // console.log('Start!');
         io.emit('url', url, password);
     }
