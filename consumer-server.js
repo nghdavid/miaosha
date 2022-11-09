dae98ba764f7401ddf74547b2b6215a898b699a7
nghdavid <nghdavid123@gmail.com>
2022-11-09 10:26:40 +0800

    feat: Make client ask result upon connecting
    改成每次使用者建立socket連線時，都去redis查詢結果，再將結果發給使用者。

--- consumer-server.js
+++ consumer-server.js
@@ -6,12 +6,29 @@ const { createAdapter } = require('@socket.io/redis-adapter');
 const app = require('./consumer-app');
 const { checkPayment } = require('./consumer/payment-queue');
 const { consumer } = require('./consumer/people-queue');
+const { issuePayJWT } = require('./util/auth');
+const Queue = require('./model/queue-model');
+const { STATUS } = require('./util/status');
 const httpServer = createServer(app);
 const { pubClient, subClient } = require('./config/redis-cluster');
-
 const { CONSUMER_PORT_TEST, CONSUMER_PORT, NODE_ENV } = process.env;
 const { socketAuth } = require('./util/auth');
 const port = NODE_ENV == 'test' ? CONSUMER_PORT_TEST : CONSUMER_PORT;
+
+let price;
+let productId;
+// Connect to Redis
+(async () => {
+    try {
+        setTimeout(async () => {
+            price = await Queue.getPrice();
+            productId = await Queue.getProductId();
+        }, 1000);
+    } catch (err) {
+        console.error('Cannot connect to Redis');
+        console.error(err);
+    }
+})();
 
 const io = new Server(httpServer, {
     cors: {
@@ -39,13 +56,33 @@ io.use(async (socket, next) => {
     }
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log('a user connected');
     console.log('user id is ', socket.userId);
     console.log('user email is ', socket.email);
     console.log('user name is ', socket.name);
     console.log('');
     socket.join(socket.userId);
+    let status = await Queue.getStatus(socket.userId);
+    if (status !== null) {
+        console.debug('使用者來詢問了');
+        // 代表使用者有成功送出搶購請求
+        status = Number(status);
+        if (status === STATUS.SUCCESS) {
+            console.debug('你有搶購成功喔');
+            io.to(socket.userId).emit('notify', STATUS.SUCCESS);
+            const accessToken = issuePayJWT({
+                id: socket.userId,
+                name: socket.name,
+                email: socket.email,
+                price,
+                productId,
+            });
+            io.to(socket.userId).emit('jwt', accessToken);
+        } else {
+            io.to(socket.userId).emit('notify', status);
+        }
+    }
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
