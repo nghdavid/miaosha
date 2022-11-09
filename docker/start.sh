7566a48e207cc6018fb203ca5b61b53e42bc8670
nghdavid <nghdavid123@gmail.com>
2022-11-09 08:54:10 +0800

    chore: Disable redis server

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ip=$(ipconfig getifaddr en0) docker-compose -f ~/Desktop/redis-cluster/docker-compose.yml up -d --build
-redis-server --daemonize yes
+#redis-server --daemonize yes
 docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.10-management
 docker-compose -f ~/Desktop/phpMyAdmin/docker-compose.yml up -d
