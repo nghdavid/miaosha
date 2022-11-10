49dcc8c3dbbb73364f4baff6ee882644fd55e1fb
nghdavid <nghdavid123@gmail.com>
2022-11-10 20:34:19 +0800

    feat: Add DNS

--- public/js/header.js
+++ public/js/header.js
@@ -1,3 +1,8 @@
 const serverIP = 'localhost';
 const apiVersion = '1.0';
 const ip = window.location.host;
+const PUBLISHER_DNS = 'http://localhost:3000';
+const CONSUMER_DNS = 'http://localhost:4000';
+const DNS = 'http://localhost:4000';
+const GENERAL_DNS = 'http://localhost:4000';
+// const DNS = 'https://miaosha/';
