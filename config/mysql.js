1bb53e37b14bd75101e9b7942593db681dba64be
nghdavid <nghdavid123@gmail.com>
2022-11-16 17:36:36 +0800

    feat: Add read replica config

--- config/mysql.js
+++ config/mysql.js
@@ -11,8 +11,19 @@ const options = {
     queueLimit: 0,
 };
 
+const readOptions = {
+    host: process.env.READ_DB_HOST,
+    user: process.env.READ_DB_USERNAME,
+    password: process.env.READ_DB_PASSWORD,
+    database: process.env.READ_DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+};
+
 // use createPool to reduce the time spent connecting to the MySQL server
 // by reusing a previous connection
 const db = mysql.createPool(options);
+const readDb = mysql.createPool(readOptions);
 
-module.exports = db;
+module.exports = { db, readDb };
