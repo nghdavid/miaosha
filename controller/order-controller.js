f742ddddde5896451cf425bb23ae858b1498aa19
nghdavid <35591407+nghdavid@users.noreply.github.com>
2022-11-16 21:51:42 +0800

    feat: Check user status first

--- controller/order-controller.js
+++ controller/order-controller.js
@@ -8,16 +8,23 @@ const { generateOrderNum } = require('../util/order');
 const checkout = async (req, res, next) => {
     const { userId, price, productId } = req;
     const { phone, address } = req.body;
+    const status = await Queue.getStatus(userId);
+    if (status === STATUS.PAID) {
+        return res.status(400).json({ error: '你已經買過了' });
+    }
+    if (status !== STATUS.SUCCESS) {
+        return res.status(400).json({ error: '你沒有購買資格' });
+    }
     const userData = await Order.getUserData(userId); // { name: 'david', email: 'test1@test.com' }
     const { name, email } = userData;
     const orderNum = generateOrderNum(1000000, 6);
-    const status = 1;
+    const payStatus = 1;
     await Promise.all([Queue.setStatus(userId, STATUS.PAID), Order.addTransaction()]);
-    const checkoutResult = await Order.addOrder(userId, price, productId, phone, address, name, email, orderNum, status);
+    const checkoutResult = await Order.addOrder(userId, price, productId, phone, address, name, email, orderNum, payStatus);
     if (checkoutResult.error) {
-        return res.status(500).json({ error: 'Payment fail' });
+        return res.status(400).json({ error: '購買失敗' });
     }
-    return res.status(200).json({ message: 'success' });
+    return res.status(200).json({ message: '購買成功' });
 };
 
 module.exports = {
