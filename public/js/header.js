fa2a5c8a0204353ded54a540040c044a05cdbed8
nghdavid <35591407+nghdavid@users.noreply.github.com>
2022-11-20 16:43:15 +0800

    feat: Add status and question

--- public/js/header.js
+++ public/js/header.js
@@ -1,11 +1,16 @@
-const serverIP = 'localhost';
+const STATUS = {
+    FAIL: -1,
+    STANDBY: 0,
+    SUCCESS: 1,
+    PAID: 2,
+};
 const apiVersion = '1.0';
-const ip = window.location.host;
-const PUBLISHER_DNS = 'http://localhost:3000';
+const question = 1;
+// const PUBLISHER_DNS = 'http://localhost:3000';
 const CONSUMER_DNS = 'http://localhost:4000';
-const DNS = 'http://localhost:4000';
+// const DNS = 'http://localhost:4000';
 const GENERAL_DNS = 'http://localhost:4000';
-// const PUBLISHER_DNS = 'https://publisher.miaosha.click';
+const PUBLISHER_DNS = 'https://publisher.miaosha.click';
 // const CONSUMER_DNS = 'https://consumer.miaosha.click';
-// const DNS = 'https://miaosha.click';
+const DNS = 'https://miaosha.click';
 // const GENERAL_DNS = 'https://consumer.miaosha.click';
