2f4d890af0b45f8be801760709df1d56b9bc96ff
nghdavid <nghdavid123@gmail.com>
2022-12-01 09:59:13 +0800

    fix: Increase waiting time

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -2,6 +2,7 @@
 mkdir mysql/datadir
 mkdir rabbitmq/rabbit-data
 docker-compose -f ./rabbitmq/docker-compose.yml up -d
-sleep 10
+# Wait rabbitmq start 
+sleep 20
 docker-compose -f ./docker-compose.yml up -d
 
