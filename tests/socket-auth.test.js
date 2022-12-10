b55cc87c59c8a0c1c7324fb22e1c651908f3c412
nghdavid <35591407+nghdavid@users.noreply.github.com>
2022-12-10 10:27:02 +0800

    test: Write unit test for socket authentication

---
+++ tests/socket-auth.test.js
@@ -0,0 +1,36 @@
+const { socketAuth } = require('../util/auth');
+describe('Test socket authentication', () => {
+    let jwt;
+    let user;
+    beforeAll(async () => {
+        // jwt token is from https://jwt.io/
+        jwt = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRGF2aWQiLCJpZCI6IjEiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20ifQ.sAJAAbOO_XkrlkJKYoJAVkpTsPU3eUFWgl96Qx2dS5Q';
+        user = await socketAuth(jwt);
+    });
+
+    test('Socket authentication id', () => {
+        expect(user.id).toBe('1');
+    });
+
+    test('Socket authentication email', () => {
+        expect(user.email).toBe('test@test.com');
+    });
+
+    test('Socket authentication name', () => {
+        expect(user.name).toBe('David');
+    });
+
+    test('Socket authentication empty jwt', async () => {
+        const wrongUser = await socketAuth('');
+        expect(wrongUser).toBeInstanceOf(Error);
+        expect(wrongUser.message).toBe('請先登入！');
+    });
+
+    test('Socket authentication wrong jwt', async () => {
+        const wrongJwt =
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRGF2aWQiLCJpZCI6IjEiLCJlbWFpbCI6InRabdAdGVzdC5jb20ifQ.sAJAAbOO_XkrlkabYoJAVkpTsPU3eUFWgl96Qx2dS5Q';
+        const wrongUser = await socketAuth(wrongJwt);
+        expect(wrongUser).toBeInstanceOf(Error);
+        expect(wrongUser.message).toBe('請重新登入！');
+    });
+});
