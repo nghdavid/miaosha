2eab1b4fbb2d68f0cdb3e24f3b6cd7d6c9c81d81
nghdavid <35591407+nghdavid@users.noreply.github.com>
2022-12-13 22:43:27 +0800

    feat: Add image

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ const ttl = hour * 60 * 60 * 1000;
 if (getWithExpiry('has_come') === null) {
     setWithExpiry('has_come', 1, ttl);
     Swal.fire({
-        icon: 'info',
-        title: '規則說明',
+        title: '網站說明',
+        html: `<img width="400px" src="image/rules.png" alt=""/>`
     });
 }
